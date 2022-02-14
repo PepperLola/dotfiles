716dd15cbab7714cbeb96baf3c211e96ce34286d
PepperLola <PepperLola@users.noreply.github.com>
2022-02-13 23:23:39 -0800

    added tmux config

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
-#!/bin/bash -e
-
 # TODO add colors
-echo "[*] Creating nvim ~/.config/nvim directory"
+echo "\033[1;93m[\033[1;97m*\033[1;93m] Creating nvim ~/.config/nvim directory"
 mkdir -p ~/.config/nvim
 
-echo "[*] Copying nvim config to ~/.config/nvim"
+echo "[\033[1;97m*\033[1;93m] Copying nvim config to ~/.config/nvim"
 sudo cp -r ./nvim ~/.config/nvim
-echo "[*] Creating a Python virtual environment"
+echo "[\033[1;97m*\033[1;93m] Creating a Python virtual environment"
 python3 -m venv ~/.config/nvim/env
 source ~/.config/nvim/env/bin/activate
-echo "[*] Upgrading pip"
+echo "[\033[1;97m*\033[1;93m] Upgrading pip"
 ~/.config/nvim/env/bin/python3 -m pip install --upgrade pip --quiet
-echo "[*] Installing Python dependencies"
+echo "[\033[1;97m*\033[1;93m] Installing Python dependencies"
 pip install wheel yapf doq pynvim --quiet
 deactivate
 
 if [ -f ~/.config/nvim/init.vim ]; then
-    echo '[*] Backing up existing init.vim'
+    echo '[\033[1;97m*\033[1;93m] Backing up existing init.vim'
     cp ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup
 fi
+
+echo "[\033[1;97m*\033[1;93m] Copying tmux config"
+cp ./tmux/.tmux.conf ~/
 
 echo "Done!"
