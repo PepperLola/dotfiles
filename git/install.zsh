c26a60f67a83071c61360f50f4dfdea11a86ec29
PepperLola <PepperLola@users.noreply.github.com>
2022-03-30 21:27:41 -0700

    added git config

---
+++ git/install.zsh
@@ -0,0 +1,20 @@
+if [ -f "$HOME/.gitconfig" ]; then
+  echo "$PC[$SC*$PC] Found ~/.gitconfig, creating backup..."
+  cp $HOME/.gitconfig $HOME/.gitconfig.bk
+fi
+
+if [ -f "$HOME/.gitignore_global" ]; then
+  echo "$PC[$SC*$PC] Found ~/.gitignore_global, creating backup..."
+  cp $HOME/.gitignore_global $HOME/.gitignore_global.bk
+fi
+
+echo "$PC[$SC*$PC] Copying .gitconfig and .gitignore_global"
+if [[ "$(pwd)" == *git ]]; then
+  cp ./.gitconfig $HOME
+  cp ./.gitignore_global $HOME
+  cp -r ./* $HOME
+else
+  cp ./git/.gitconfig $HOME
+  cp ./git/.gitignore_global $HOME
+  cp -r ./git/* $HOME
+fi
