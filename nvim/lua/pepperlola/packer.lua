c004773ad720f6a174b0c4ab90f4fc7a6e43cdd2
PepperLola <PepperLola@users.noreply.github.com>
2023-04-07 09:42:31 -0700

    feat(plugins): added nvim-surround

--- nvim/lua/pepperlola/packer.lua
+++ nvim/lua/pepperlola/packer.lua
@@ -80,4 +80,13 @@ return require('packer').startup(function(use)
             require("copilot").setup({})
         end, }
     use 'eandrju/cellular-automaton.nvim'
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 end)
