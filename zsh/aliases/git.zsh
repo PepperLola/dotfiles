8dac40a45ce0c1bffbc1312b735ec7ed3168968b
PepperLola <PepperLola@users.noreply.github.com>
2023-07-18 15:27:44 -0700

    fix(alias): switch gcsm to commit gpg signed rather than commit signoff

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -1,14 +1,4 @@
-alias gp="git push -u origin $(git rev-parse --abbrev-ref HEAD)"
-alias gpl="git pull"
-alias ga="git add $1"
-alias gad="ga ."
-alias gc="git commit -am $1"
-alias gd="git diff"
-alias gst="git status"
-alias gr="git restore $1"
-alias grs="gr --staged $1"
-alias grd="gr ."
-alias grds="grd ."
+alias gcsm="git commit --gpg-sign --message"
 
 # git restore unity files I don't want to push
 alias gru="git restore *.csproj engine.sln UserSettings/EditorUserSettings.asset"
