daaf067e41d587ec189fd47062731086220bba1c
PepperLola <PepperLola@users.noreply.github.com>
2023-07-21 11:10:53 -0700

    feat(backup) quiet, fix backup

--- nvim/install.zsh
+++ nvim/install.zsh
@@ -12,19 +12,26 @@ fi
 
 if [ -d $HOME/.config/nvim ]; then
     echo "$PC[$SC*$PC] Backing up existing nvim config"
-    zip $HOME/.config/nvim.bk.zip $HOME/.config/nvim/* && \
-        rm -rf $HOME/.config/nvim/after
+    if [ -f $HOME/.config/nvim.bk.zip ]; then
+        rm $HOME/.config/nvim.bk.zip
+    fi
+    WD="$(pwd)"
+    cd $HOME/.config
+    zip -qr nvim.bk.zip ./nvim/* && \
+        rm -rf ./nvim/after && \
+        cd $WD
 fi
 
 echo "$PC[$SC*$PC] Copying nvim config to ~/.config/nvim"
-$CONF_PATH=./nvim/
+CONF_PATH=./nvim/
 if [[ "$(pwd)" == *nvim ]]; then
   CONF_PATH=./
 fi
 
-rsync -av --progress $CONF_PATH/* $HOME/.config/nvim --exclude after && \
+echo "$PC[$SC*$PC] Installing nvim packages"
+rsync -avq $CONF_PATH/* $HOME/.config/nvim --exclude after && \
     nvim --headless -c "so|autocmd User PackerComplete quitall" ~/.config/nvim/lua/pepperlola/packer.lua -c "PackerSync" && \
-    rsync -av --progress $CONF_PATH/after $HOME/.config/nvim
+    rsync -avq $CONF_PATH/after $HOME/.config/nvim
 
 
 if [[ -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]]; then
