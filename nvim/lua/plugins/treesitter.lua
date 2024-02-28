fb747d97bba75330324c804d732f9c66a2ddf0eb
PepperLola <PepperLola@users.noreply.github.com>
2024-02-28 15:27:01 -0800

    feat: added nvim-treesitter-textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+        },
         build = ':TSUpdate',
         config = function()
             require("nvim-treesitter.install").prefer_git = true
@@ -12,8 +16,81 @@ return {
                     enable = true,
                     additional_vim_regex_highlighting = false,
                 },
+                indent = { enable = true },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
             }
         end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        lazy = true,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ['aa'] = '@parameter.outer',
+                            ['ia'] = '@parameter.inner',
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                            ['ai'] = '@conditional.outer',
+                            ['ii'] = '@conditional.inner',
+                            ['al'] = '@loop.outer',
+                            ['il'] = '@loop.inner',
+                            ['at'] = '@comment.outer',
+                        }
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                            [']o'] = '@loop.*',
+                        },
+                        goto_next_end = {
+                            [']M'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[M'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
+                        goto_next = {
+                            [']i'] = '@conditional.inner',
+                        },
+                        goto_previous = {
+                            ['[i'] = '@conditional.inner',
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ['<leader>a'] = '@parameter.inner',
+                        },
+                        swap_previous = {
+                            ['<leader>A'] = '@parameter.inner',
+                        },
+                    },
+                },
+            })
+        end
     },
     'nvim-treesitter/playground',
 }
