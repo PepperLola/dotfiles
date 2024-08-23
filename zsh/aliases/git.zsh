d9b0a2242f809e0e0e17b698f1998a29f69b9259
PepperLola <PepperLola@users.noreply.github.com>
2024-08-22 23:46:09 -0700

    feat: added alias to open all merge conflicts in nvim

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -3,3 +3,6 @@ alias gc="gcsm"
 
 # git restore unity files I don't want to push
 alias gru="git restore *.csproj engine.sln UserSettings/EditorUserSettings.asset"
+function gemc() {
+  files="$(git status | rg -o 'both modified: *(.*)$' -or '$1')" && qflist=$(xargs -I{} rg --with-filename --line-number --no-heading "<{4,}" "{}" <<< "$files" | awk -F: '{ printf "{\"filename\": \"%s\", \"lnum\": \"%s\"},", $1, $2, $3 }' | sed -E 's/.$//g' | awk '{ print "call setqflist([" $0 "])" }') && nvim -c "$qflist" -c "copen" "$files"
+}
