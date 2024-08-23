402f2520fe67ad18ab54da1e070e721ba8fc469d
PepperLola <PepperLola@users.noreply.github.com>
2024-08-23 00:07:27 -0700

    feat: added message when no conflicts are found

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -4,5 +4,5 @@ alias gc="gcsm"
 # git restore unity files I don't want to push
 alias gru="git restore *.csproj engine.sln UserSettings/EditorUserSettings.asset"
 function gemc() {
-  files="$(git status | rg -o 'both modified: *(.*)$' -or '$1')" && qflist=$(xargs -I{} rg --with-filename --line-number --no-heading "<{4,}" "{}" <<< "$files" | awk -F: '{ printf "{\"filename\": \"%s\", \"lnum\": \"%s\"},", $1, $2, $3 }' | sed -E 's/.$//g' | awk '{ print "call setqflist([" $0 "])" }') && nvim -c "$qflist" -c "copen" "$files"
+  files="$(git status | rg -o 'both modified: *(.*)$' -or '$1')" && [[ -n "$files" ]] && {qflist=$(xargs -I{} rg --with-filename --line-number --no-heading "<{4,}" "{}" <<< "$files" | awk -F: '{ printf "{\"filename\": \"%s\", \"lnum\": \"%s\"},", $1, $2, $3 }' | sed -E 's/.$//g' | awk '{ print "call setqflist([" $0 "])" }') && nvim -c "$qflist" -c "copen" "$files"} || echo "No conflicts found!"
 }
