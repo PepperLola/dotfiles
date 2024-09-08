d2db5b26e57f6b0d35cae5f5876d9e71bb046857
PepperLola <PepperLola@users.noreply.github.com>
2024-09-07 18:37:52 -0700

    fix: updated typst preview

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -8,7 +8,6 @@ return {
     {
         "chomosuke/typst-preview.nvim",
         ft = "typst",
-        version = "0.1.*",
         build = function() require "typst-preview".update() end,
     },
     {
