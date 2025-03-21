d8c6531ab6620cf8560cff0b8e8d417f13c9ed78
PepperLola <PepperLola@users.noreply.github.com>
2025-03-21 14:39:43 -0700

    chore: updated nvim packages and fixed typo

--- nvim/lua/pepperlola/remap.lua
+++ nvim/lua/pepperlola/remap.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertlca resize +2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 -- Move blocks of code in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
