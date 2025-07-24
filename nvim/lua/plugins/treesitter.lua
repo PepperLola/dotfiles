482178b0a642319e51ee27c08c8594272af3854f
PepperLola <PepperLola@users.noreply.github.com>
2025-07-24 16:13:24 -0700

    feat: add parameter jumps to nvim textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -59,18 +59,22 @@ return {
                             [']m'] = '@function.outer',
                             [']]'] = '@class.outer',
                             [']o'] = '@loop.*',
+                            [']a'] = '@parameter.outer',
                         },
                         goto_next_end = {
                             [']M'] = '@function.outer',
                             [']['] = '@class.outer',
+                            [']A'] = '@parameter.outer',
                         },
                         goto_previous_start = {
                             ['[m'] = '@function.outer',
                             ['[['] = '@class.outer',
+                            ['[a'] = '@parameter.outer',
                         },
                         goto_previous_end = {
                             ['[M'] = '@function.outer',
                             ['[]'] = '@class.outer',
+                            ['[A'] = '@parameter.outer',
                         },
                         goto_next = {
                             [']i'] = '@conditional.inner',
